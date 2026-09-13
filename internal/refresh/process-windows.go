24a3458f2a3cf4b2c4500d13ae4e2f5daf8410bd
Amaan <texamaan86@gmail.com>
2026-09-13 23:38:10 +0530

    fix(tests): fix windows tests pipeline

--- internal/refresh/process-windows.go
+++ internal/refresh/process-windows.go
@@ -4,10 +4,15 @@ package refresh
 
 import "syscall"
 
+const (
+	detachedProcess = 0x00000008
+	createNoWindow   = 0x08000000
+)
+
 func detachedProcessAttributes() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{
 		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP |
-			syscall.DETACHED_PROCESS |
-			syscall.CREATE_NO_WINDOW,
+			detachedProcess |
+			createNoWindow,
 	}
 }
